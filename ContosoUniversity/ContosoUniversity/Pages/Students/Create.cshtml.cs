86fdd2567c2522d5c35ffe448ebae948821d0b1f
rick-anderson <riande@microsoft.com>
2017-10-16 15:13:43 -1000

    model.Student[0].LastName

--- ContosoUniversity/ContosoUniversity/Pages/Students/Create.cshtml.cs
+++ ContosoUniversity/ContosoUniversity/Pages/Students/Create.cshtml.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using ContosoUniversity.Data;
-using ContosoUniversity.Models;
+using System;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Pages.Students
 {
@@ -34,17 +30,30 @@ namespace ContosoUniversity.Pages.Students
         [BindProperty]
         public Student Student { get; set; }
 
+        #region snippet_OnPostAsync
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+            #region snippet_TryUpdateModelAsync
 
-            _context.Students.Add(Student);
-            await _context.SaveChangesAsync();
+            var emptyStudent = new Student();
 
-            return RedirectToPage("./Index");
+            if (await TryUpdateModelAsync<Student>(
+                emptyStudent,
+                "student",   // Prefix for form value.
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+            {
+                #endregion
+                _context.Students.Add(emptyStudent);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
         }
+        #endregion
     }
 }
--- ContosoUniversity/ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections.Generic;
+#define Sort // first 
+
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Data;
-using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Pages.Students
 {
@@ -19,11 +19,48 @@ namespace ContosoUniversity.Pages.Students
             _context = context;
         }
 
-        public IList<Student> Student { get;set; }
+       public IQueryable<Student> Student { get; set; }
 
+#if first
+        public IList<Student> Student { get; set; }
+
+        #region snippet_ScaffoldedIndex
         public async Task OnGetAsync()
         {
             Student = await _context.Students.ToListAsync();
         }
+        #endregion
+#endif
+
+#if Sort
+        #region snippet_sort
+        public async Task OnGetAsync(string sortOrder)
+        {
+            ViewData["NameSort"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSort"] = sortOrder == "Date" ? "date_desc" : "Date";
+            Student = from s in _context.Students
+                           select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    Student = Student.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    Student = Student.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    Student = Student.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    Student = Student.OrderBy(s => s.LastName);
+                    break;
+            }
+            await Student.AsNoTracking().ToListAsync();
+
+        }
+        #endregion
+#endif
+
     }
 }
